1d286d022eb816c18e0228a3cc9e15d516c66720
onedrako <onedrako@gmail.com>
2022-05-16 22:27:40 -0500

    Tab navigation working

--- components/Header/SideNavigations.tsx
+++ components/Header/SideNavigations.tsx
@@ -1,14 +1,89 @@
-import { useContext, ReactNode } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { ThemeContext } from '@contexts/ThemeContext'
+import { ElementSelectedOnTabNav } from '@customTypes/types'
 
-const SideNavigation = ({ children }: {children: ReactNode}) => {
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const initialState = {
+  home: false,
+  portfolio: false,
+  certifications: false,
+  personal: false,
+  about: false
+}
+
+const SideNavigation = () => {
+  const [selectedNavigation, setSelectedNavigation] = useState<ElementSelectedOnTabNav>(initialState)
+
+  const route = useRouter()
+
   const { theme } = useContext(ThemeContext)
+
+  const handleSelectedNavigation = () => {
+    let actualRoute = route.pathname.split('/')[1]
+    if (actualRoute === '') {
+      actualRoute = 'home'
+    }
+    const navigationObject: ElementSelectedOnTabNav = { ...selectedNavigation }
+
+    const keys = Object.keys(navigationObject) as string[]
+    keys.forEach(key => {
+      if (key === actualRoute) {
+        navigationObject[key as keyof ElementSelectedOnTabNav] = true
+      } else {
+        navigationObject[key as keyof ElementSelectedOnTabNav] = false
+      }
+    })
+    setSelectedNavigation(navigationObject)
+  }
+
+  useEffect(() => {
+    handleSelectedNavigation()
+  }, [route.pathname])
 
   return (
     <>
       <div className='SideNavigation'>
         <ul>
-          {children}
+        <li className={'SideNavigation__li'} style={
+              {
+                // color: `${selectedNavigation.home ? 'blue' : '#ffffff'}`,
+                borderColor: `${selectedNavigation.home ? 'blue' : '#ffffff'}`
+              }}>
+              <Link href="/"><a>Home</a></Link>
+            </li>
+            <li className={'SideNavigation__li'} style={
+              {
+                // color: `${selectedNavigation.home ? 'blue' : '#ffffff'}`,
+                borderColor: `${selectedNavigation.portfolio ? 'blue' : '#ffffff'}`
+              }}>
+              <Link href="/portfolio"><a>Portfolio</a></Link>
+            </li>
+
+            <li className={'SideNavigation__li'} style={
+              {
+                // color: `${selectedNavigation.home ? 'blue' : '#ffffff'}`,
+                borderColor: `${selectedNavigation.certifications ? 'blue' : '#ffffff'}`
+              }}>
+              <Link href="certifications">Certifications</Link>
+            </li>
+
+            <li className={'SideNavigation__li'} style={
+              {
+                // color: `${selectedNavigation.home ? 'blue' : '#ffffff'}`,
+                borderColor: `${selectedNavigation.personal ? 'blue' : '#ffffff'}`
+              }}>
+              <Link href="personal">Personal</Link>
+            </li>
+
+            <li className={'SideNavigation__li'} style={
+              {
+                // color: `${selectedNavigation.home ? 'blue' : '#ffffff'}`,
+                borderColor: `${selectedNavigation.about ? 'blue' : '#ffffff'}`
+              }}>
+              <Link href="about">About Me</Link>
+            </li>
         </ul>
       </div>
       <style jsx>{`
@@ -19,6 +94,13 @@ const SideNavigation = ({ children }: {children: ReactNode}) => {
           top: 15px;
           background: ${theme.modalBackgroundColor};
           padding: 25px;
+        }
+        .SideNavigation__li {
+          width: 60%;
+          font-size: 4rem;
+          display: flex;
+          align-items: center;
+          border-bottom: 4px solid #fff;
         }
         ul {
           padding: 0;
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,22 +1,13 @@
 import { useContext, ReactNode } from 'react'
-import { useRouter } from 'next/router'
 
 import { ThemeContext } from '@contexts/ThemeContext'
 import Header from '../Header/Header'
 
 const Layout = ({ children, conditionForStyle }: {children: ReactNode, conditionForStyle: boolean }) => {
   const { theme } = useContext(ThemeContext)
-  const route = useRouter()
-
-  let actualRoute = route.pathname.split('/')[1]
-
-  if (actualRoute === '') {
-    actualRoute = 'home'
-  }
-
   return (
     <>
-      <Header actualRoute={actualRoute}/>
+      <Header />
       {children}
 
       <style jsx global>{`
